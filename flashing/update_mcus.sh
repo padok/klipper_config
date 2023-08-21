eacb9be2068fdef338dd8342b0861b8a9d8a77a5
Padok <padok@disroot.org>
2023-08-21 15:29:48 +0100

    update klipper and add klicky probe

--- flashing/update_mcus.sh
+++ flashing/update_mcus.sh
@@ -7,4 +7,4 @@ make flash FLASH_DEVICE=/dev/serial/by-id/usb-Klipper_lpc1768_10200001881C4AAFE6
 # Turbo
 cp -f ~/klipper_config/flashing/btt-skr-turbo-v1.4.config ./.config
 #make flash FLASH_DEVICE=1d50:6015
-make flash FLASH_DEVICE=/dev/serial/by-id/usb-Klipper_lpc1769_1A80011605A5D79572874662C52000F5-if00
+make flash FLASH_DEVICE=/dev/serial/by-id/usb-Klipper_lpc1769_2810011505A5D795897A4662C52000F5-if00
